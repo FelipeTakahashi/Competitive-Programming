539e71c234f36aa49e8760d366a0a071f935d930
Felipe Jun Takahashi <131722952+FelipeTakahashi@users.noreply.github.com>
2023-05-09 23:11:41 -0300

    1005.cpp

--- CPP/Iniciante/1005.cpp
+++ CPP/Iniciante/1005.cpp
@@ -7,11 +7,11 @@ int main() {
 
     double a, b;
 
-    cin  >> a;
-    cin  >> b;
+        cin  >> a;
+        cin  >> b;
 
-    cout << fixed << setprecision(5);
-    cout << "MEDIA = " << ((3.5*a) + (7.5*b))/11 << "\n";
+        cout << fixed << setprecision(5);
+        cout << "MEDIA = " << ((3.5*a) + (7.5*b))/11 << "\n";
 
     return 0;
 
