1b790ca88c93065de5f5fed5711a0108e547ba18
Felipe Jun Takahashi <131722952+FelipeTakahashi@users.noreply.github.com>
2023-05-16 01:13:00 -0300

    1016.cpp

---
+++ CPP/Iniciante/1016.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int carro_um;
+
+        cin  >> carro_um;
+    
+    cout << carro_um * 2 << " minutos" << endl;
+
+    return 0;
+    
+}
