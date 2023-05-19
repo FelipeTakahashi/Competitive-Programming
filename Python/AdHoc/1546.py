81928fd6058db1ddbefed287a12bb354ac066f1c
Felipe Jun Takahashi <131722952+FelipeTakahashi@users.noreply.github.com>
2023-05-19 17:11:58 -0300

    1546.py

---
+++ Python/AdHoc/1546.py
@@ -0,0 +1,17 @@
+dias = int(input())
+for i in range(dias):
+    caso = int(input())
+    for j in range(caso):
+        numero = int(input())
+
+        if numero == 1:
+            print("Rolien")
+    
+        elif numero == 2:
+            print("Naej")
+    
+        elif numero == 3:
+            print("Elehcim")
+    
+        elif numero == 4:
+            print("Odranoel")
