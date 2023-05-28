2409c48d4cce547a6cc74d1ea2d86cd81011761a
Felipe Jun Takahashi <131722952+FelipeTakahashi@users.noreply.github.com>
2023-05-28 15:54:38 -0300

    1186.cpp

---
+++ CPP/Iniciante/1186.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <iomanip>
+
+int main() {
+    char operation;
+        std::cin >> operation;
+    
+    double M[12][12], value, answer = 0;
+
+        for(int row = 0; row < 12; row++) {
+            for (int column = 0; column < 12; column++) {
+                std::cin >> value;
+                M[row][column] = value;
+                    if (row + column > 11) {
+                        answer += value;
+                    }
+            }
+        }
+
+    if (operation == 'S') {
+        std::cout << std::fixed << std::setprecision(1) << answer << std::endl;
+    }
+
+    else if (operation == 'M') {
+        std::cout << std::fixed << std::setprecision(1) << answer/66.0 << std::endl;
+    }
+}
