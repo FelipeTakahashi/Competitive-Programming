763d9aac06217f45d2a23cf1e6846575d658bf30
Felipe Jun Takahashi <131722952+FelipeTakahashi@users.noreply.github.com>
2023-05-29 02:16:15 -0400

    1924.cpp

---
+++ CPP/Iniciante/1924.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    int many;
+        std::cin >> many;
+    std::string course;
+        for (int i = 0; i < many; i++) {
+            std::cin >> course;
+        }
+    std::cout << "Ciencia da Computacao" << std::endl;
+}
